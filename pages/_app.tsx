d94ad905d071ecf9cd599c19b7b9fcb6853e5068
Yuki Yamamura <yuki.yamamura@icloud.com>
2022-05-15 09:37:14 +0900

    chore: remove unnecessary code
    - Arrange formats in React components
    - Make pages simple as soon as possible

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,9 @@
+import type { AppProps } from 'next/app';
+
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const App = ({ Component, pageProps }: AppProps) => (
+  <Component {...pageProps} />
+);
 
-export default MyApp
+export default App;
