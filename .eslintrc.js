03faeb00f397d258047157d88a4663a0d472e675
Yuki Yamamura <yuki.yamamura@icloud.com>
2022-05-15 11:24:10 +0900

    chore: configure Prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,6 +16,7 @@ module.exports = {
     'plugin:import/recommended',
     'plugin:import/typescript',
     'next/core-web-vitals',
+    'prettier'
   ],
   rules: {
     '@typescript-eslint/no-unused-vars': 'off',
